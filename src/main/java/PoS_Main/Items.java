339df9e6fa8a33005a6676986af5e81e5e7f266f
Tieelo <tieel@gmx.net>
2023-03-18 00:49:42 +0100

    Complete POM with SQLite support

---
+++ src/main/java/PoS_Main/Items.java
@@ -0,0 +1,50 @@
+package PoS_Main;
+
+public class Items {
+
+    private int id;
+    private String name;  //name of the item
+    private String measurement; //unit of measurement
+    private double price; // price of the drink
+    private double stock; //
+    private String group;
+
+    public Items(int id, String name, String measurement, double price, double stock, String group) {
+        this.id = id;
+        this.name = name;
+        this.measurement = measurement;
+        this.price = price;
+        this.stock = stock;
+        this.group = group;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public double getStock() {
+        return stock;
+    }
+    public boolean decreaseStock(double amount) {
+        if (amount <= stock){
+            stock -=amount;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    @Override
+    public String toString() {
+        return String.format("%-15s %-15s %.2f€, %.2f %s im Inventar", group, name, price, stock, measurement);
+    }
+
+}
+
