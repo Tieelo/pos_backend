8e36622a3eee5ad6ee155a367e7d0643cf736246
Tieelo <tieel@gmx.net>
2023-03-19 23:59:06 +0100

    added requirement in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,10 @@ module com.example.demo {
     requires javafx.fxml;
             
         requires org.controlsfx.controls;
-                    requires org.kordamp.ikonli.javafx;
-            requires org.kordamp.bootstrapfx.core;
-            
-    opens com.example.demo to javafx.fxml;
+        requires org.kordamp.ikonli.javafx;
+        requires org.kordamp.bootstrapfx.core;
+        requires java.sql;
+
+        opens com.example.demo to javafx.fxml;
     exports com.example.demo;
 }
