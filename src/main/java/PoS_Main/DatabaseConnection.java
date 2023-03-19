c706564adfb7fdccb3099e06c61f47395acbe1bb
Tieelo <tieel@gmx.net>
2023-03-20 00:59:36 +0100

    prettier

--- src/main/java/PoS_Main/DatabaseConnection.java
+++ src/main/java/PoS_Main/DatabaseConnection.java
@@ -5,22 +5,23 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private Connection conn;
 
-    public DatabaseConnection(String url) {
-        try {
-            Class.forName("org.sqlite.JDBC");
-            conn = DriverManager.getConnection(url);
-            conn.setAutoCommit(false);
-        } catch (SQLException e) {
-            System.out.println("Failed to connect to database.");
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
+  private Connection conn;
 
-    public Connection getConnection() {
-        return conn;
+  public DatabaseConnection(String url) {
+    try {
+      Class.forName("org.sqlite.JDBC");
+      conn = DriverManager.getConnection(url);
+      conn.setAutoCommit(false);
+    } catch (SQLException e) {
+      System.out.println("Failed to connect to database.");
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
     }
+  }
+
+  public Connection getConnection() {
+    return conn;
+  }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,13 @@
 module com.example.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-        requires org.controlsfx.controls;
-        requires org.kordamp.ikonli.javafx;
-        requires org.kordamp.bootstrapfx.core;
-        requires java.sql;
+  requires javafx.controls;
+  requires javafx.fxml;
 
-        opens com.example.demo to javafx.fxml;
-    exports com.example.demo;
+  requires org.controlsfx.controls;
+  requires org.kordamp.ikonli.javafx;
+  requires org.kordamp.bootstrapfx.core;
+  requires java.sql;
+  requires org.xerial.sqlitejdbc;
+
+  opens com.example.demo to javafx.fxml;
+  exports com.example.demo;
 }
