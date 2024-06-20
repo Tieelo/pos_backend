a0bae7b5e31e1527429f7e688768db1d9f55f614
mirco.franzkowiak <mirco.franzkowiak@zurich.com>
2024-06-20 09:03:22 +0200

    Add new allowed origin in CorsConfig
    A new allowed origin has been added to the CORS configuration in CorsConfig.java. Additional IPv6 origin `http://[2003:103:818:22a0::50]:8080` has been appended to the list of accepted requests, enhancing the compatibility with different network environments.

--- src/main/java/org/pos_backend/model/objects/CorsConfig.java
+++ src/main/java/org/pos_backend/model/objects/CorsConfig.java
@@ -10,7 +10,16 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**")
-				.allowedOrigins("https://posweb.tieelo.de:444", "https://oauth.tieelo.de:444", "https://pos.tieelo.de:8080", "http://localhost:8080","https://pos.tieelo.de:444", "http://192.168.10.14:8080", "http://[2003:103:818:22a0::51]:8080" )
+				.allowedOrigins(
+						"https://posweb.tieelo.de:444",
+						"https://oauth.tieelo.de:444",
+						"https://pos.tieelo.de:8080",
+						"http://localhost:8080",
+						"https://pos.tieelo.de:444",
+						"http://192.168.10.14:8080",
+						"http://[2003:103:818:22a0::51]:8080",
+						"http://[2003:103:818:22a0::50]:8080"
+				)
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
 				.allowedHeaders("Content-Type", "Authorization", "*")
 				.allowCredentials(true)
