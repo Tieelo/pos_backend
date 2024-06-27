e164adf44df437d6253004f0105c487d1fca3dc9
mirco.franzkowiak <mirco.franzkowiak@zurich.com>
2024-06-27 11:04:50 +0200

    Add new allowed origin in CorsConfig
    A new allowed origin has been added to the CORS configuration in CorsConfig.java. Additional IPv6 origin `http://[2003:103:818:22a0::50]:8080` has been appended to the list of accepted requests, enhancing the compatibility with different network environments.

--- src/main/java/org/pos_backend/model/objects/CorsConfig.java
+++ src/main/java/org/pos_backend/model/objects/CorsConfig.java
@@ -11,11 +11,13 @@ public class CorsConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**")
 				.allowedOrigins(
+						"https://pos.tieelo.de",
+						"https://posweb.tieelo.de",
+						"https://pos.tieelo.de:444",
 						"https://posweb.tieelo.de:444",
 						"https://oauth.tieelo.de:444",
 						"https://pos.tieelo.de:8080",
 						"http://localhost:8080",
-						"https://pos.tieelo.de:444",
 						"http://192.168.10.14:8080",
 						"http://[2003:103:818:22a0::50]",
 						"http://192.168.110.50"
